524f9a901dd593498396234ad3ee386039671ae4
hyojin-k <hyo.khj@gmail.com>
2022-02-09 13:52:25 +0900

    [feat] detail page
    [feat] detail page

--- meetup-project/pages/[meetupId]/index.js
+++ meetup-project/pages/[meetupId]/index.js
@@ -1,6 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function Detail() {
-  return <div></div>;
+  return (
+      <MeetupDetail 
+        image = 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        title = 'First Meetup'
+        address = 'somewhere'
+        description = 'A first Meetup'
+      />
+  )
 }
 
 export default Detail;
