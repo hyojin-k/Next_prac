fb612bba975cbbd006e328cb607f84394cb03567
hyojin-k <hyo.khj@gmail.com>
2022-02-13 00:02:47 +0900

    [feat] api request
    [feat] api request

--- meetup-project/pages/new-meetup/index.js
+++ meetup-project/pages/new-meetup/index.js
@@ -1,8 +1,25 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
-  function addMeetuphandler(enteredMeetupData){
-    console.log(enteredMeetupData)
+  const router = useRouter();
+
+  async function addMeetuphandler(enteredMeetupData){
+    // console.log(enteredMeetupData)
+
+    // api/new-meetup 으로 request 전송
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push('/');
   }
   return (
     <NewMeetupForm onAddMeetup = {addMeetuphandler} />
